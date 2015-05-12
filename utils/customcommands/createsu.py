23686c12269226a9140a40b7df30d6891bdbd9bd
Seonggyu Lee <shalomeir@gmail.com>
2015-05-12 21:00:53 +0900

    aws

---
+++ utils/customcommands/createsu.py
@@ -0,0 +1,11 @@
+__author__ = 'shalomeir'
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "admin@admin.com", "admin")
