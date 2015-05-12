1efd136fa7dea4980a8a15124b86a1fcb9732e67
Seonggyu Lee <shalomeir@gmail.com>
2015-05-12 20:25:29 +0900

    aws

--- manage.py
+++ manage.py
@@ -3,11 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    try:
-        param1 = os.environ['PARAM1']
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snippod_boilerplate.settings.prod")
-    except KeyError :
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snippod_boilerplate.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snippod_boilerplate.settings.dev")
 
     from django.core.management import execute_from_command_line
 
