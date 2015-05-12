1d07936fd8bfed6d8ea9776a18ad58ddf1e64eee
Seonggyu Lee <shalomeir@gmail.com>
2015-05-12 17:46:03 +0900

    aws

--- snippod_boilerplate/settings/prod.py
+++ snippod_boilerplate/settings/prod.py
@@ -20,7 +20,8 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$6(x*g_2g9l_*g8peb-@anl5^*8q!1w)k&e&2!i)t6$s8kik75'
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # Application definition
