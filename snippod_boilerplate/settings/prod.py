2a3587430e85bdfd07e7791db576a2049ddcf352
Seonggyu Lee <shalomeir@gmail.com>
2015-05-12 17:43:16 +0900

    aws

---
+++ snippod_boilerplate/settings/prod.py
@@ -0,0 +1,85 @@
+"""
+Django settings for snippod boilerplate project.
+
+This is a base starter for snippod.
+
+For more information on this file, see
+https://github.com/shalomeir/snippod-boilerplate
+
+"""
+from snippod_boilerplate.settings.common import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$6(x*g_2g9l_*g8peb-@anl5^*8q!1w)k&e&2!i)t6$s8kik75'
+
+
+# Application definition
+
+INSTALLED_APPS += (
+    'storages',
+)
+
+# MIDDLEWARE_CLASSES += (
+#     '',
+# )
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
+
+DATABASE_OPTIONS = {'charset': 'utf8'}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_ROOT = 'staticfiles'
+
+AWS_STORAGE_BUCKET_NAME = "snippod-boilerplate-bucket"
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
+
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', True)
+
+#MEDIA FILE (user uploaded files)
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "www", "media")
+
+# TEMPLATE_DIRS = (
+#     os.path.join(BASE_DIR, 'djangoapps/templates'),
+# )
