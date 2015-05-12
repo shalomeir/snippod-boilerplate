42b674cf1584fed5cff12bb0bb92a1b9e80321e9
Seonggyu Lee <shalomeir@gmail.com>
2015-05-12 23:44:52 +0900

    aws

--- djangoapps/main/management/commands/createsu.py
+++ djangoapps/main/management/commands/createsu.py
@@ -1,11 +1,11 @@
 import os
 
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from authentication.models import Account
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+        if not Account.objects.filter(username="admin").exists():
+            Account.objects.create_superuser("admin@admin.com", "admin", "admin")
 
