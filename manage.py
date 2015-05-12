bcc26095db1d7a6ba2d7f41b0b29c26887865381
Seonggyu Lee <shalomeir@gmail.com>
2015-05-12 17:04:55 +0900

    requirement

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snippod_boilerplate.settings.prod")
+    try:
+        param1 = os.environ['PARAM1']
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snippod_boilerplate.settings.prod")
+    except KeyError :
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snippod_boilerplate.settings.dev")
 
     from django.core.management import execute_from_command_line
 
