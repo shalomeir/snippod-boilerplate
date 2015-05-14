e43ea6a0121ca34efabfa7f717427d2451bba29e
Seonggyu Lee <shalomeir@gmail.com>
2015-05-14 16:28:15 +0900

    aws

--- djangoapps/posts/migrations/0001_initial.py
+++ djangoapps/posts/migrations/0001_initial.py
@@ -13,16 +13,70 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Post',
+            name='Comment',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('content', models.TextField()),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('content', models.CharField(max_length=110)),
+                ('created_at', models.DateTimeField(db_index=True, auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
+                'ordering': ['-created_at'],
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='CommentUpvote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('comment', models.ForeignKey(to='posts.Comment')),
+                ('voter', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=30)),
+                ('link', models.URLField()),
+                ('created_at', models.DateTimeField(db_index=True, auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['-created_at'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='PostUpvote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('post', models.ForeignKey(to='posts.Post')),
+                ('voter', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='postupvote',
+            unique_together=set([('post', 'voter')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='commentupvote',
+            unique_together=set([('comment', 'voter')]),
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(to='posts.Post'),
+            preserve_default=True,
         ),
     ]
--- djangoapps/posts/models.py
+++ djangoapps/posts/models.py
@@ -1,92 +1,92 @@
-# from django.db import models
-# from django.db.models import Count
-# from authentication.models import Account
-#
-#
-# class PostQuerySet(models.QuerySet):
-#     def upvotes(self):
-#         return self.annotate(Count('postupvote'))\
-#             .order_by('-postupvote__count','-created_at')
-#
-#     def newest(self):
-#         return self.order_by('-created_at')
-#
-#     def comments(self):
-#         return self.annotate(Count('comment'))\
-#             .order_by('-comment__count','-created_at')
-#
-#
-# class Post(models.Model):
-#     author = models.ForeignKey(Account)
-#     title = models.CharField(max_length=30)
-#     link = models.URLField()
-#
-#     created_at = models.DateTimeField(db_index=True, auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
-#     objects = models.Manager()
-#     sorted_objects = PostQuerySet.as_manager()
-#
-#     def __str__(self):
-#         return '{0}'.format(self.title)
-#
-#     def _get_comment_count(self):
-#         return self.comment_set.count()
-#     comment_count = property(_get_comment_count)
-#
-#     def _get_upvote_count(self):
-#         return self.postupvote_set.count()
-#     upvote_count = property(_get_upvote_count)
-#
-#     class Meta:
-#         ordering = ['-created_at']
-#
-#
-# class CommentQuerySet(models.QuerySet):
-#     def upvotes(self):
-#         return self.annotate(Count('commentupvote'))\
-#             .order_by('-commentupvote__count','-created_at')
-#
-#     def newest(self):
-#         return self.order_by('-created_at')
-#
-#
-# class Comment(models.Model):
-#     post = models.ForeignKey(Post)
-#     author = models.ForeignKey(Account)
-#     content = models.CharField(max_length=110)
-#     created_at = models.DateTimeField(db_index=True, auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
-#     objects = models.Manager()
-#     sorted_objects = CommentQuerySet.as_manager()
-#
-#     def __str__(self):
-#         return '{0}'.format(self.content)
-#
-#     def _get_upvote_count(self):
-#         return self.commentupvote_set.count()
-#     upvote_count = property(_get_upvote_count)
-#
-#     class Meta:
-#         ordering = ['-created_at']
-#
-#
-# class PostUpvote(models.Model):
-#     post = models.ForeignKey(Post)
-#     voter = models.ForeignKey(Account)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#
-#     class Meta:
-#         unique_together = ('post', 'voter')
-#
-#
-# class CommentUpvote(models.Model):
-#     comment = models.ForeignKey(Comment)
-#     voter = models.ForeignKey(Account)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#
-#     class Meta:
-#         unique_together = ('comment', 'voter')
-#
-#
+from django.db import models
+from django.db.models import Count
+from authentication.models import Account
+
+
+class PostQuerySet(models.QuerySet):
+    def upvotes(self):
+        return self.annotate(Count('postupvote'))\
+            .order_by('-postupvote__count','-created_at')
+
+    def newest(self):
+        return self.order_by('-created_at')
+
+    def comments(self):
+        return self.annotate(Count('comment'))\
+            .order_by('-comment__count','-created_at')
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Account)
+    title = models.CharField(max_length=30)
+    link = models.URLField()
+
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = models.Manager()
+    sorted_objects = PostQuerySet.as_manager()
+
+    def __str__(self):
+        return '{0}'.format(self.title)
+
+    def _get_comment_count(self):
+        return self.comment_set.count()
+    comment_count = property(_get_comment_count)
+
+    def _get_upvote_count(self):
+        return self.postupvote_set.count()
+    upvote_count = property(_get_upvote_count)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class CommentQuerySet(models.QuerySet):
+    def upvotes(self):
+        return self.annotate(Count('commentupvote'))\
+            .order_by('-commentupvote__count','-created_at')
+
+    def newest(self):
+        return self.order_by('-created_at')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(Account)
+    content = models.CharField(max_length=110)
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = models.Manager()
+    sorted_objects = CommentQuerySet.as_manager()
+
+    def __str__(self):
+        return '{0}'.format(self.content)
+
+    def _get_upvote_count(self):
+        return self.commentupvote_set.count()
+    upvote_count = property(_get_upvote_count)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class PostUpvote(models.Model):
+    post = models.ForeignKey(Post)
+    voter = models.ForeignKey(Account)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'voter')
+
+
+class CommentUpvote(models.Model):
+    comment = models.ForeignKey(Comment)
+    voter = models.ForeignKey(Account)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('comment', 'voter')
+
+
