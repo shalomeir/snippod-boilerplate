e96d5e50c8e4cccab5355300038e746ba1f907b7
Seonggyu Lee <shalomeir@gmail.com>
2015-05-18 19:10:12 +0900

    increase post title field lengh

---
+++ djangoapps/posts/migrations/0002_auto_20150518_1009.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('posts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='title',
+            field=models.CharField(max_length=110),
+            preserve_default=True,
+        ),
+    ]
