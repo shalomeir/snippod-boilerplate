c4a155b9488422ff5f6caa9bb562385745d8c7d2
Seonggyu Lee <shalomeir@gmail.com>
2016-01-25 21:27:07 +0900

    fix eslint

--- snippod-webapp/api/utils/url.js
+++ snippod-webapp/api/utils/url.js
@@ -7,12 +7,12 @@ export function mapUrl(availableActions = {}, url = []) {
     return notFound;
   }
   /*eslint-disable */
-  const reducer = (next, current) => {
-    if (next.action && next.action[current]) {
-      return {action: next.action[current], params: []}; // go deeper
+  const reducer = (prev, current) => {
+    if (prev.action && prev.action[current]) {
+      return {action: prev.action[current], params: []}; // go deeper
     } else {
-      if (typeof next.action === 'function') {
-        return {action: next.action, params: next.params.concat(current)}; // params are found
+      if (typeof prev.action === 'function') {
+        return {action: next.action, params: prev.params.concat(current)}; // params are found
       } else {
         return notFound;
       }
@@ -20,7 +20,7 @@ export function mapUrl(availableActions = {}, url = []) {
   };
   /*eslint-enable */
 
-  const actionAndParams = url.reduce(reducer, {action: availableActions, params: []});
+  const actionAndParams = url.reduce(reducer, { action: availableActions, params: [] });
 
   return (typeof actionAndParams.action === 'function') ? actionAndParams : notFound;
 }
